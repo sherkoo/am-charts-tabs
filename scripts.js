2e49cce7e2298cdbc58c074093673fe6828d9346
Stephen <stephen@pvp-live.com>
2014-09-22 14:20:38 -0500

    files added

---
+++ scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+  // vars
+  var tabMenu = $('.tabs .menu li'),
+      tabContent = $('.tabs .content');
+
+  // initialize
+  tabMenu.first().addClass('active');
+  tabContent.not(":first").hide();
+
+  // when user clicks
+  tabMenu.click(function(e){
+    tabMenu.removeClass('active');
+    $(this).addClass('active');
+    var id = $(e.target).index();
+    tabContent.hide().eq(id).fadeIn(300);
+  });
+
+});
